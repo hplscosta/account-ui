5104784942916863a031cc963fbb82ddb358a395
hugo.costa <hugo.costa.tek@gmail.com>
2017-06-04 19:27:21 +0100

    enable feign client on spring boot app

--- src/main/java/com/demo/account/ui/AccountUiApplication.java
+++ src/main/java/com/demo/account/ui/AccountUiApplication.java
@@ -2,11 +2,15 @@ package com.demo.account.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableConfigurationProperties( AccountUiApplicationProperties.class )
 public class AccountUiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountUiApplication.class, args);
+	public static void main( String[] args ) {
+		SpringApplication.run( AccountUiApplication.class, args );
 	}
 }
