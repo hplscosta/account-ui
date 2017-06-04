975f92682c6c3bcb3827f4a973f2725b9b3d6734
hugo.costa <hugo.costa.tek@gmail.com>
2017-06-04 19:26:06 +0100

    fix create method annotations

--- src/main/java/com/demo/account/ui/controller/account/client/AccountServiceClient.java
+++ src/main/java/com/demo/account/ui/controller/account/client/AccountServiceClient.java
@@ -4,9 +4,7 @@ import com.demo.account.ui.controller.account.Account;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Sends request to Account Service API
@@ -14,10 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author Hugo Costa
  * @since 1.0.0
  */
-@FeignClient( url = "${account-service.url}" )
-@RequestMapping( path = "/account", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE )
+@FeignClient( name = "account", url = "${demo.account-service.url}" )
+@RequestMapping( consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE )
 public interface AccountServiceClient {
 
-	@PostMapping
-	ResponseEntity<Account> create( @RequestBody Account account );
+	@RequestMapping( value = "/account", method = RequestMethod.POST )
+	ResponseEntity<Account> create( Account account );
 }
