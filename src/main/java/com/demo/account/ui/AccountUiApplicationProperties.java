2aa6bb4a1b1184c898ef4d7fad6fa4a6ebbf4c19
hugo.costa <hugo.costa.tek@gmail.com>
2017-06-04 15:22:58 +0100

    add application properties

---
+++ src/main/java/com/demo/account/ui/AccountUiApplicationProperties.java
@@ -0,0 +1,23 @@
+package com.demo.account.ui;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Application properties
+ *
+ * @author Hugo Costa
+ * @since 1.0.0
+ */
+@Data
+@ConfigurationProperties( prefix = "demo" )
+public class AccountUiApplicationProperties {
+
+	private AccountServiceProperties accountService = new AccountServiceProperties();
+
+	@Data
+	public static class AccountServiceProperties {
+
+		private String topic = "accounts";
+	}
+}
