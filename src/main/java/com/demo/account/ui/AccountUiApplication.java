bb03a748fc8cb75133615d407cac0e2e84514c46
hugo.costa <hugo.costa.tek@gmail.com>
2017-06-04 13:29:42 +0100

    create spring boot project with starters (web, redis and websockets)

---
+++ src/main/java/com/demo/account/ui/AccountUiApplication.java
@@ -0,0 +1,12 @@
+package com.demo.account.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountUiApplication.class, args);
+	}
+}
