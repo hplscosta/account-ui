5a8406a84e7581a063482d703e5e870976ae5c54
hugo.costa <hugo.costa.tek@gmail.com>
2017-06-05 21:07:24 +0100

    change url path of feign client

--- src/main/java/com/demo/account/ui/controller/account/receiver/api/AccountServiceClient.java
+++ src/main/java/com/demo/account/ui/controller/account/receiver/api/AccountServiceClient.java
@@ -1,10 +1,11 @@
-package com.demo.account.ui.controller.account.client;
+package com.demo.account.ui.controller.account.receiver.api;
 
 import com.demo.account.ui.controller.account.Account;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Sends request to Account Service API
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
  * @author Hugo Costa
  * @since 1.0.0
  */
-@FeignClient( name = "account", url = "${demo.account-service.url}" )
+@FeignClient( name = "account", url = "${demo.ui.account-service.url}" )
 @RequestMapping( consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE )
 public interface AccountServiceClient {
 
