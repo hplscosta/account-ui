ae08acf9b9696baa5cd7fd45f86199156ac3e11a
hugo.costa <hugo.costa.tek@gmail.com>
2017-06-05 22:25:19 +0100

    call AccountStompService to send account data to browsers

--- src/main/java/com/demo/account/ui/controller/account/receiver/AccountReceiver.java
+++ src/main/java/com/demo/account/ui/controller/account/receiver/AccountReceiver.java
@@ -1,18 +1,15 @@
 package com.demo.account.ui.controller.account.receiver;
 
-/**
- * Created by hc on 04-06-2017.
- */
-
 import com.demo.account.ui.controller.account.Account;
-import com.demo.account.ui.controller.account.client.AccountServiceClient;
+import com.demo.account.ui.controller.account.receiver.api.AccountServiceClient;
+import com.demo.account.ui.controller.account.receiver.web.AccountStompService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * Handles message fetched from redis channel. <br>
  * Calls {@link AccountServiceClient} to persist account data.<br>
- * TODO calls XXX websocket
+ * Sends {@link Account} data to client browsers through {@link AccountStompService}.
  *
  * @author Hugo Costa
  * @since 1.0.0
@@ -21,10 +18,13 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class AccountReceiver {
 
-	private final AccountServiceClient client;
+	private final AccountServiceClient accountServiceClient;
+
+	private final AccountStompService accountStompService;
 
 	public void receive( Account account ) {
 		log.info( "Account user {} received!", account.getUser() );
-		client.create( account );
+		accountServiceClient.create( account );
+		accountStompService.send( account );
 	}
 }
